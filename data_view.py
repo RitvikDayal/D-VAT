86a093d07b0f930aeb090638b2192a60776f47c0
Ritvik Dayal <ritvikr1605@gmail.com>
2020-02-17 11:13:18 +0530

    Create data_view.py

---
+++ data_view.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from pandastable import Table, TableModel
+
+bg_color = '#394b5c'
+
+'''-------------------------------Window Configuration----------------------------------------'''
+root = Tk()
+root.title('Data Visualizer and Analyzer')
+root.iconbitmap(r'icon.ico')
+root.config(bg=bg_color)
+root.resizable(width=False, height=False)
+
+
+
+
+
+
+root.geometry('850x400+450+150')
